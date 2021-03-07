2dd25b8aae1f80307b27a83a6a0134668ac44211
twentysvn <qodim.is30@gmail.com>
2021-03-07 18:50:39 +0700

    api Food

---
+++ app/Http/Controllers/API/FoodController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Helpers\ResponseFormatter;
+use App\Http\Controllers\Controller;
+use App\Models\Food;
+use Illuminate\Http\Request;
+
+class FoodController extends Controller
+{
+    public function all(Request $request)
+    {
+        $id = $request->input('id');
+        $limit = $request->input('limit', 6);
+        $name = $request->input('name');
+        $types = $request->input('types');
+
+        $price_from = $request->input('price_from');
+        $price_to = $request->input('price_to');
+
+        $rate_from = $request->input('price_from');
+        $rate_to = $request->input('price_to');
+
+        if ($id) {
+            $food = Food::find($id);
+            if ($food) {
+                return ResponseFormatter::success($food, 'data produk berhasil diambil');
+            } else {
+                return ResponseFormatter::error(null, 'data produk tidak ditemukan', 404);
+            }
+        }
+
+        $food = Food::query();
+        if ($name) {
+            $food->where('name', 'like', '%' . $name . '%');
+        }
+        if ($types) {
+            $food->where('types', 'like', '%' . $types . '%');
+        }
+        if ($price_from) {
+            $food->where('price_from', '<=',  $price_from);
+        }
+        if ($price_to) {
+            $food->where('price_to', '>=',  $price_to);
+        }
+        if ($rate_from) {
+            $food->where('rate_from', '<=',  $rate_from);
+        }
+        if ($rate_to) {
+            $food->where('rate_to', '>=',  $rate_to);
+        }
+
+        return ResponseFormatter::success($food->paginate($limit), 'data produk berhasil diambil');
+    }
+}
